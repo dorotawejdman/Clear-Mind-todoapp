76971355407b3e612497d7320794caa9b1b2f8c6
dorota <dorota.wejdman@gmail.com>
2020-11-16 12:40:00 +0100

    Add select.scss, style select

--- app.js
+++ app.js
@@ -6,18 +6,19 @@ let names = {
     archiveList: '#right',
     listContainer: '#list',
     container: '#container',
-    alert:'.alert'
+    alert:'.alert',
+    priorityInput: '#priority-select'
 }
 
 //Task class
 class Task{
-    constructor(description,deadline,id=0, priority=1){
+    constructor(description,deadline,id=0, priority=3){
         this.description = description;
         this.deadline = deadline;
         this.id = id;
         this.priority = priority;
-        this.todoHTML = '<div class="task" id="task-%id%"><div><input type="checkbox" name="" id="do-task"><p>%description%</p></div><div><p class="date-tag">%date%</p><div class="check-div"><input type="checkbox" name="" class="delete remove-task"><i class="fas fa-times checkbox-icon"></i></div></div></div>';
-        this.archHTML = '<div class="task" id="task-%id%"><div><p>%description%</p></div><div><div class="check-div"><input type="checkbox" name="" class="delete remove-task"><i class="fas fa-times checkbox-icon"></i></div></div></div>';
+        this.todoHTML = '<div class="task priority-%priority-number%" id="task-%id%"><div><input type="checkbox" name="" id="do-task"><p>%description%</p></div><div><p class="date-tag">%date%</p><input type="checkbox" name="" class="delete remove-task"></div></div>';
+        this.archHTML = '<div class="task" id="task-%id%"><div><p>%description%</p></div><div><input type="checkbox" name="" class="delete remove-task"></div></div>';
         
         //add priority
     }
@@ -26,24 +27,42 @@ class Task{
 //UI class
 class UI{
     static displayTasks(){
-        const tasks = Store.getTasks();
-        tasks.forEach((task)=>UI.addTaskToList(task));
+        const tasks = StoreToDo.getTasks();
+        tasks.forEach((task)=>UI.addTaskToList(task,'todo'));
+        const tasks2 = StoreDone.getTasks();
+        tasks2.forEach((task)=>UI.addTaskToList(task,'done'));
 
     }
-    static addTaskToList(task){
-        const toDoList = document.querySelector(names.todoList);
-        
-        //archivalList = document.querySelector(DOMnames.archiveList);c
-        task.todoHTML = task.todoHTML.replace('%description%', task.description)
-        task.todoHTML = task.todoHTML.replace('%id%', task.id)
-        
-        task.todoHTML = task.todoHTML.replace('%date%', task.deadline)
+    static addTaskToList(task, type){
+        let html = 'todoHTML';
+        let List = document.querySelector(names.todoList);
+        if (type == 'todo'){
+            List = document.querySelector(names.todoList);
+            html = 'todoHTML';
+            task[html] = task[html].replace('%date%', task.deadline)
+            task[html] = task[html].replace('%priority-number%', task.priority)
+        }
+            
+        else{
+            List = document.querySelector(names.archiveList);
+            html = 'archHTML';
+        }
+        task[html] = task[html].replace('%description%', task.description)
+        task[html] = task[html].replace('%id%', task.id)
 
-        toDoList.insertAdjacentHTML('beforeend', task.todoHTML);
+        List.insertAdjacentHTML('beforeend', task[html]);
     }
     static deleteTask(el) {
         if(el.classList.contains('delete')){
-            el.parentElement.parentElement.parentElement.remove();
+            console.log(el.parentElement.parentElement.parentElement)
+            if(el.parentNode.classList.contains('task'))
+                el.parentElement.remove();
+            else
+                el.parentElement.parentElement.remove();
+        }
+        else if (el.id=='do-task'){
+            console.log(el.parentElement.parentElement)
+            el.parentElement.parentElement.remove();
         }
         UI.showAlert('Task removed','info');
     }
@@ -64,37 +83,59 @@ class UI{
 }
 //Store class
 class Store {
-
-    static getTasks(type) {
+    static getTasks() {
         let tasks;
-        if(localStorage.getItem('tasks') === null){
+        const name = this.prototype.constructor.name
+        if(localStorage.getItem(name) === null){
             tasks = [];
         }
         else{
-            tasks = JSON.parse(localStorage.getItem('tasks'));
+            tasks = JSON.parse(localStorage.getItem(name));
         }
         return tasks;
     }
-
+    static getSpecificTask(domID){
+        const id = domID.split('-')[1];
+        const tasks = this.getTasks();
+        let out 
+        tasks.forEach((task,index)=>{
+            if(task.id == id){
+                out = task;
+            }
+        });
+        return out;
+    }
     static addTask(task) {
-        const tasks = Store.getTasks();
+        const tasks = this.getTasks();
         tasks.push(task);
-        localStorage.setItem('tasks',JSON.stringify(tasks))
+        const name = this.prototype.constructor.name
+        localStorage.setItem(name,JSON.stringify(tasks))
     }
 
-    static removeTask(task) {
-        const tasks = Store.getTasks();
-
-        tasks.forEach((task, id)=>{
-            if(task.id === id){
-                tasks.splice(id,1);
+    static removeTask(domID) {
+        let tasks = this.getTasks();
+        const id = domID.split('-')[1];
+        const name = this.prototype.constructor.name
+        //console.log(name)
+        //console.log(tasks[0], ' almost deleted')
+        tasks.forEach((task, index)=>{
+            //console.log(task , ' in for')
+            if(task.id == id){
+                tasks.splice(index,1);
+                //console.log(tasks, ' left')
+                
             }
         });
 
-        localStorage.setItem('tasks', JSON.stringify(tasks));
+        localStorage.setItem(name, JSON.stringify(tasks));
     }
 }
-
+class StoreToDo extends Store{
+    
+}
+class StoreDone extends Store{
+    
+}
 
 //Event: display tasks
 document.addEventListener('DOMContentLoaded', UI.displayTasks);
@@ -107,14 +148,14 @@ document.querySelector(names.confirmInput).addEventListener('click', e=>{
     //Get values
     const description = document.querySelector(names.descriptionInput).value;
     const deadline = document.querySelector(names.deadlineInput).value;
-    const priority = document.querySelector(names.deadlineInput).value; //TODO - dodac submenu do wyboru priorytetu
+    const priority = document.querySelector(names.priorityInput).value; //TODO - dodac submenu do wyboru priorytetu
 
     //Validation
     if(description === ''){
         UI.showAlert('Please fill the field.','warning');
     }
     // Instantiate task
-    let tasks = Store.getTasks()
+    let tasks = StoreToDo.getTasks()
     let ids=[];
 
     //Counting ID
@@ -130,27 +171,52 @@ document.querySelector(names.confirmInput).addEventListener('click', e=>{
     }
     
     //New Task
-    const task = new Task(description, deadline, id)  //TODO dodac tu priorytet 
-
+    const task = new Task(description, deadline, id, priority)  //TODO dodac tu priorytet 
     //Add task to UI
-    UI.addTaskToList(task);
-
+    UI.addTaskToList(task,'todo');
     //Add task to store
-    Store.addTask(task);
-
+    StoreToDo.addTask(task);
     //Clear teh field
     document.querySelector(names.descriptionInput).value = null;
     
 })
-//Event: remove a task
-document.querySelector(names.listContainer).addEventListener('click', (e)=>{
+//Delete task from ToDO List
+document.querySelector(names.todoList).addEventListener('click', (e)=>{
 
-    //Remove task from UI
-    UI.deleteTask(e.target);
+    //target doest have delete then 'do task' 
+    if( !(e.target.className.includes('delete'))){
+        //Get accurate task
+        const taskID = e.target.parentNode.parentNode.id
+        
+        const task = StoreToDo.getSpecificTask(taskID);
+        console.log(task)
+        //Add task to UI
+        UI.addTaskToList(task,'done');
+        //Add task to store
+        StoreDone.addTask(task);
+        //Remove task from UI
+        console.log(e.target)
+        UI.deleteTask(e.target);//dziala tylko dla krzyzyka
+        //Remove task from the Store
+        domID = e.target.parentNode.parentNode.id
+        StoreToDo.removeTask(domID); 
 
-    //Remove task from the Store
-    id = e.target.parentNode.parentNode.parentNode.id
-    Store.removeTask(id); 
-    //console.log(e.target.parentNode.parentNode.parentNode.id);
-}
-)
+    }
+    else{
+        //Remove task from UI
+        UI.deleteTask(e.target);
+        //Remove task from the Store
+        domID = e.target.parentNode.parentNode.id;
+        StoreToDo.removeTask(domID); 
+    }
+    
+});
+//Delete task from Archival List
+document.querySelector(names.archiveList).addEventListener('click', (e)=>{
+
+        //Remove task from UI
+        UI.deleteTask(e.target);
+        //Remove task from the Store
+        domID = e.target.parentNode.parentNode.id
+        StoreDone.removeTask(domID);
+});
